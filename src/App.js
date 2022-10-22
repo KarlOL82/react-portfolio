e3476d311b4d93975544cdc3c213542f10f0c8fe
KarLOL82 <klinfeldt@gmail.com>
2022-10-21 22:27:04 -0700

    copypasta template with some customization

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-teal-400">
-      Hello world!
-    </h1>
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
   );
 }
 

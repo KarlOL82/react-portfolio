d6996aecc387edd10b0044208cd04f779023d5e9
KarLOL82 <klinfeldt@gmail.com>
2022-10-22 09:23:46 -0700

    changed bio blurb a little

--- src/components/About.js
+++ src/components/About.js
@@ -7,8 +7,7 @@ export default function About() {
             <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
               <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
                 Hi, I'm Karl.
-                <br className="hidden lg:inline-block" /> I love to build amazing
-                apps.
+                <br className="hidden lg:inline-block" /> I build apps with Javascript.
               </h1>
               <p className="mb-8 leading-relaxed">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
@@ -19,7 +18,7 @@ export default function About() {
                 <a
                   href="#contact"
                   className="inline-flex text-white bg-teal-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-                  Reach Out
+                  Contact Me
                 </a>
                 <a
                   href="#projects"

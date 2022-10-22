efda83c8991ffef3a4687d3d612707ed69f7c9fc
KarLOL82 <klinfeldt@gmail.com>
2022-10-21 23:00:54 -0700

    some basic styling

--- src/components/About.js
+++ src/components/About.js
@@ -18,12 +18,12 @@ export default function About() {
               <div className="flex justify-center">
                 <a
                   href="#contact"
-                  className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+                  className="inline-flex text-white bg-teal-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
                   Reach Out
                 </a>
                 <a
                   href="#projects"
-                  className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+                  className="ml-4 inline-flex text-white bg-teal-500 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
                   See My Apps and Projects
                 </a>
               </div>
@@ -32,7 +32,7 @@ export default function About() {
               <img
                 className="object-cover object-center rounded"
                 alt="headshot"
-                src="../src/assets/proPic.png"
+                src="./assets/proPic.png"
               />
             </div>
           </div>

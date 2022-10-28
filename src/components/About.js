b94a14a44b54490faf7ffdc50c82e51937d5d8d0
KarLOL82 <klinfeldt@gmail.com>
2022-10-27 21:20:19 -0700

    changed image

--- src/components/About.js
+++ src/components/About.js
@@ -33,7 +33,7 @@ export default function About() {
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
           <img
-            className="object-cover object-center rounded-lg"
+            className="object-cover object-center z-0 rounded-lg"
             alt="headshot"
             src={proPic}
           />

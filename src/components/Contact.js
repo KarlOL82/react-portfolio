75aec34f7625db5cb98d8c2bf567db417ad23ecc
KarLOL82 <klinfeldt@gmail.com>
2022-12-03 20:49:08 -0800

    testing contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,32 +1,36 @@
-import React from "react";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import twitterIcon from "../images/twitterIcon.svg";
 import linkedinLogo from "../images/linkedinLogo.svg";
 import githubLogo from "../images/githubLogo.svg";
 
 // Function to render contact section with form, map, links, and info
-export default function Contact() {
-  const [name, setName] = React.useState("");
-  const [email, setEmail] = React.useState("");
-  const [message, setMessage] = React.useState("");
-  console.log(name, email, message);
-  function encode(data) {
-    return Object.keys(data)
-      .map(
-        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-      )
-      .join("&");
-  }
+// export default function Contact() {
+//   const [name, setName] = React.useState("");
+//   const [email, setEmail] = React.useState("");
+//   const [message, setMessage] = React.useState("");
+//   console.log(name, email, message);
+//   function encode(data) {
+//     return Object.keys(data)
+//       .map(
+//         (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+//       )
+//       .join("&");
+//   }
 
-  function handleSubmit(e) {
+export default function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
     e.preventDefault();
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", name, email, message }),
-    })
-      .then(() => alert("Message sent!"))
-      .catch((error) => alert(error));
-  }
+
+    emailjs.sendForm('service_4r21yyp', 'template_4tu40ci', form.current, 'kT0ZWuqBEo0IrgC8P')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   return (
     <section id="contact" className="relative">
       <div className="container px-5 pt-36 pb-16 mx-auto flex sm:flex-nowrap flex-wrap">
@@ -91,7 +95,8 @@ export default function Contact() {
         <form
           // renders contact form
           name="contact"
-          onSubmit={handleSubmit}
+          ref={form}
+          onSubmit={sendEmail}
           
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
@@ -108,9 +113,9 @@ export default function Contact() {
             <input
               type="text"
               id="name"
-              name="name"
+              name="user_name"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setName(e.target.value)}
+              // onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div className="relative mb-4">
@@ -120,9 +125,9 @@ export default function Contact() {
             <input
               type="email"
               id="email"
-              name="email"
+              name="user_email"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setEmail(e.target.value)}
+              // onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="relative mb-4">
@@ -135,11 +140,12 @@ export default function Contact() {
               id="message"
               name="message"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setMessage(e.target.value)}
+              // onChange={(e) => setMessage(e.target.value)}
             />
           </div>
           <button
             type="submit"
+            value="send"
             className="text-white bg-indigo-400 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-500 rounded text-lg">
             Submit
           </button>
